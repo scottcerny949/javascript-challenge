b7048e14b3e18d8f80201da8be1cefba68beebae
Scott Cerny <scottcerny@yahoo.com>
2021-06-23 14:34:54 -0700

    settign up input and filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,3 +7,29 @@ var button = d3.select("#filter-btn");
 
 var form = d3.select("#form");
 
+var table = d3.select("#ufo-table");
+
+console.log(tableData);
+
+button.on("click", dataEnter);
+form.on("submit", dataEnter);
+
+function dataEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Print the value to the console
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+
+    console.log(filteredData);
+
+    
+}
